9495db4a199a95345f63d0f58f0f09daa40b79a0
Vikash7566 <vikashchoudhary798@gmail.com>
2023-03-27 00:32:32 +0530

    basic

---
+++ src/HomeScreen.js
@@ -0,0 +1,56 @@
+import React from 'react'
+import './HomeScreen.css'
+import Nav from './Nav'
+
+function HomeScreen() {
+  return (
+    <div className='homeScreen'>
+      <Nav/>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+      <h1>Hello</h1>
+    </div>
+  )
+}
+
+export default HomeScreen
---
+++ src/Nav.js
@@ -0,0 +1,37 @@
+import React, { useEffect, useState } from "react";
+import "./Nav.css";
+
+function Nav() {
+    const [show, setShow] = useState(false)
+    const transitionNavbar =()=>{
+        if(window.screenY > 100){
+        setShow(true)
+        }else{
+            setShow(false)
+        }
+    }
+
+    useEffect(()=>{
+        window.addEventListener("scroll",transitionNavbar);
+        // return()=> window.removeEventListener("scroll",transitionNavbar);
+    },[])
+
+  return (
+    <div className= {`nav ${show && "nav__black" } `}>
+      <div className="nav__content">
+        <img
+          className="nav__logo"
+          src="https://download.logo.wine/logo/Netflix/Netflix-Logo.wine.png"
+          alt=""
+        />
+        <img
+          className="nav__avatar"
+          src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Nav;
