fb1f67477b8e4265e2b957c079868e509e4527d7
Vikash325 <ifour.vikash@gmail.com>
2023-06-23 18:23:44 +0530

    added banner content

--- src/Banner.js
+++ src/Banner.js
@@ -1,15 +1,26 @@
-import React from 'react'
-import "./Banner.css"
+import React from "react";
+import "./Banner.css";
 
 export default function Banner() {
   return (
-    <header className='Banner' style={{
+    <header
+      className="Banner"
+      style={{
         backgroundSize: "cover",
-        backgroundImage : `url("https://whatchareading.com/wp-content/uploads/2015/04/Netflix-Marvels-Daredevil-Banner.jpg")`,
-        objectFit:"content",
-        backgroundPositon:"center center",
-    }}>
-      
+        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
+        objectFit: "content",
+        backgroundPositon: "center center",
+      }}
+    >
+      <div className="banner__contents">
+        <h1 className="banner__ttitle">Movie</h1>
+        <div className="banner_buttons">
+          <button className="banner__btn">play</button>
+          <button className="banner__btn">my list</button>
+        </div>
+        <div className="banner__description">this is the banner description</div>
+        <div className="banner__fadeBottom"/>
+      </div>
     </header>
-  )
+  );
 }
